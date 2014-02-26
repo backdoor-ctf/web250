981049b19ffec05a8273eff289add42dc5714c93
Abhay Rana <capt.n3m0@gmail.com>
2014-02-26 15:19:51 +0530

    Initial commit.

---
+++ routes/index.js
@@ -0,0 +1,32 @@
+
+/*
+ * GET home page.
+ */
+var md = require( "markdown" ).markdown;
+var yml = require('js-yaml');
+Mustache=require("mustache");
+
+exports.index = function(req, res){
+  res.sendfile("public/index.html");
+};
+
+exports.convert = function(req, res){
+  var markdown = req.body.markdown;
+  var yaml = '';
+  var data = null;
+  if(markdown.slice(0,3)==='---')
+  {
+    var end = markdown.indexOf('---',3);
+    if(end>-1){
+        yaml = markdown.slice(3,end);
+        data = yml.load(yaml);
+        markdown = markdown.slice(end+3);
+    }
+    //No closing --- were found
+    else
+        markdown=markdown.slice(3);
+  }
+  //Now we template it up:
+  var content = Mustache.render(markdown, data)
+  res.send(md.toHTML(content));
+};
